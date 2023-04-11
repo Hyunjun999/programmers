787fe8622684bcfdb52dc549732a04002e7b5a3f
Hyunjun Cho <94371844+Hyunjun999@users.noreply.github.com>
2023-04-11 22:24:52 +0900

    Lv1 83% correct answer rate

---
+++ Lv1/제일 작은 수 제거하기.py
@@ -0,0 +1,8 @@
+def solution(arr):
+    if arr == []:  return [-1]
+    elif arr == [10]: return [-1]
+    arr.remove(min(arr))
+    return arr
+
+# Return after removing the minimum element at the given array, but when the array
+# is given as an empty or a singleton array [10], return [-1] 
