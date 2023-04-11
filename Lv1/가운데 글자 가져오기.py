8f2fdb5a10f31c8832d82bca81770e5dd0e6abdd
Hyunjun Cho <94371844+Hyunjun999@users.noreply.github.com>
2023-04-11 22:40:53 +0900

    Lv1 83% correct answer rate

---
+++ Lv1/가운데 글자 가져오기.py
@@ -0,0 +1,5 @@
+def solution(s):
+    return s[len(s) // 2] if len(s) % 2 == 1 else s[len(s) // 2 - 1: len(s) // 2 + 1]
+
+# Return the middle charater or string of the 's'. If the length of 's' is odd, return the middle character
+# else return the 2 middle string
